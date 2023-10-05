5e61e47219e30c296761084ca23a09b7a9969ba6
VinyEditor <vinyeditor@gmail.com>
2023-10-04 21:07:31 -0300

    Adicionando JPA na entrega CP2

---
+++ src/main/java/br/com/fiap/petshop/domain/entity/servico/Consulta.java
@@ -0,0 +1,26 @@
+package br.com.fiap.petshop.domain.entity.servico;
+
+import br.com.fiap.petshop.domain.entity.animal.Animal;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TB_CONSULTA")
+@DiscriminatorValue("CONSULTA")
+public class Consulta extends Servico {
+    public Consulta() {
+    }
+
+    public Consulta(Long id, BigDecimal valor, LocalDateTime abertura, LocalDateTime autorizacao, LocalDateTime conclusao, String descricao, String observacao, Animal animal) {
+        super(id, valor, abertura, autorizacao, conclusao, descricao, observacao, animal);
+    }
+
+    @Override
+    public String toString() {
+        return "Consulta{} " + super.toString();
+    }
+}
